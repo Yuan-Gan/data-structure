754129e5a89da4eec8145b60ed9a74ecd1cf80b4
yuanmou-gan <2028679602@qq.com>
2024-12-09 18:23:25 +0800

    动态

--- Seqlist动态顺序表/Seqlist动态顺序表/Seqlist.c
+++ Seqlist动态顺序表/Seqlist动态顺序表/Seqlist.c
@@ -1,5 +1,100 @@
 #include"Seqlist.h"
 void SeqListInit(SeqList* ps)
 {
+	ps->a = NULL;
+	ps->capacity = ps->size = 0;
+}
 
+void SeqListPrint(SeqList *ps)
+{
+	for (int i = 0; i < ps->size; i++)
+	{
+		printf("%d ", ps->a[i]);
+	}
+	printf("\n");
+}
+void SeqlistCheckCapacity(SeqList* ps)
+{
+	if (ps->capacity == ps->size)
+	{
+		int newcapacity = ps->capacity == 0 ? 4 : 2 * ps->capacity;
+		SeqList* tmp = (SeqList*)realloc(ps->a, newcapacity * sizeof(SLDateType));
+		if (tmp == NULL)
+		{
+			exit(-1);
+		}
+		ps->a = tmp;
+		ps->capacity = newcapacity;
+	}
+	
+}
+void SeqListPushBack(SeqList* ps, SLDateType x)
+{
+	assert(ps);
+	SeqlistCheckCapacity(ps);
+	ps->a[ps->size++] = x;
+}
+void SeqListPushFront(SeqList* ps, SLDateType x)
+{
+	assert(ps);
+	SeqlistCheckCapacity(ps);
+	for (int i = ps->size; i > 0; i--)
+	{
+		ps->a[i] = ps->a[i - 1];
+	}
+	ps->a[0] = x;
+	ps->size++;
+}
+void SeqListPopFront(SeqList* ps)
+{
+	assert(ps);
+	for (int i = 0 ; i < ps->size ; i++)
+	{
+		ps->a[i] = ps->a[i + 1];
+	}
+	ps->size--;
+}
+void SeqListPopBack(SeqList* ps)
+{
+	assert(ps && ps->size);
+	ps->size--;
+
+}
+int SeqListFind(SeqList* ps, SLDateType x)
+{
+	for (int i = 0; i < ps->size; i++)
+	{
+		if (ps->a[i] == x)
+			return i;
+	}
+	return NULL;
+}
+void SeqListInsert(SeqList* ps, int pos, SLDateType x)
+{
+	assert(ps);
+	assert(pos >= 0 && pos <= ps->size);
+	SeqlistCheckCapacity(ps);
+	for (int i = ps->size; i > pos; i--)
+	{
+		ps->a[i] = ps->a[i - 1];
+	}
+	ps->a[pos] = x;
+	ps->size++;
+}
+void SeqListErase(SeqList* ps, int pos)
+{
+	assert(ps && ps->size);
+	assert(pos >= 0 && pos < ps->size);
+	for (int i = pos; i < ps->size; i++)
+	{
+		ps->a[i] = ps->a[i + 1];
+	}
+	ps->size--;
+}
+void SeqListDestroy(SeqList* ps)
+{
+	if (ps->a)
+		free(ps->a);
+	ps->a = NULL;
+	ps->size = ps->capacity = 0;
 }
--- Seqlist动态顺序表/Seqlist动态顺序表/test.c
+++ Seqlist动态顺序表/Seqlist动态顺序表/test.c
@@ -1,9 +1,65 @@
 #include"Seqlist.h"
+void test01()
+{
+	SeqList sl;
+	SeqListInit(&sl);
+	SeqListPushBack(&sl, 1);
+	SeqListPushBack(&sl, 2);
+	SeqListPushBack(&sl, 3);
+	SeqListPushBack(&sl, 4);
+	SeqListPrint(&sl);
+	SeqListPushFront(&sl, 1);
+	SeqListPushFront(&sl, 2);
+	SeqListPushFront(&sl, 3);
+	SeqListPushFront(&sl, 4);
+	SeqListPrint(&sl);
 
+	SeqListPopFront(&sl);
+	SeqListPrint(&sl);
+	SeqListPopFront(&sl);
+	SeqListPrint(&sl);
+	SeqListPopFront(&sl);
+	SeqListPrint(&sl);
+	SeqListPopFront(&sl);
+	SeqListPrint(&sl);
+	SeqListPopFront(&sl);
+	SeqListPrint(&sl);
+	SeqListPopFront(&sl);
+	SeqListPrint(&sl);
+	SeqListPopFront(&sl);
+	SeqListPrint(&sl);
+	SeqListPopFront(&sl);
+	SeqListPrint(&sl);
+	SeqListPopFront(&sl);
+	SeqListPrint(&sl);
+	SeqListPopFront(&sl);
+	SeqListPrint(&sl);
+}
+void test02()
+{
+	SeqList sl;
+	SeqListInit(&sl);
+	SeqListPushBack(&sl, 1);
+	SeqListPushBack(&sl, 4);
+	SeqListPushBack(&sl, 7);
+	SeqListPushBack(&sl, 8);
+	SeqListPrint(&sl);
+	/*
+	int pos = SeqListFind(&sl, 8);
+	if (pos != NULL)
+		printf("找到了下标是%d\n", pos);
+	else
+		printf("找不到\n");
+	SeqListInsert(&sl, 3, 9);
+	SeqListPrint(&sl);*/
+	SeqListErase(&sl, 1);
+	SeqListPrint(&sl);
+
+}
 int main()
 {
+	//test01();
 
-
-
+	test02();
 	return 0;
 }
